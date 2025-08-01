5891b8fd7a19e8fe3f07b3e320a246b8ccbb871e
marjorie40 <marjoriemazeau2@gmail.com>
2025-08-01 11:51:01 +0200

    MaJ 010825 pm

--- training/afpa/CDA24060/Exo10_MMAU.java
+++ training/afpa/CDA24060/Exo10_MMAU.java
@@ -1,4 +1,32 @@
 package training.afpa.CDA24060;
 
 public class Exo10_MMAU {
+// TabMultiplication
+
+    public int TabMultiplication (){
+        int x;
+        int y;
+        int LIGNES = 10; // x 
+        int COLONNES = 10; // y
+
+
+        // Creation de tableau 10*10
+        /* Boucle avec :
+        * saisie de x ;
+        * saisie de y ;
+        * calculProduit; appel d'indice dans tableau (ou calcul ?)
+        * Affichage résultat;
+        * Proposition "souhaitez vous encore un calcul ?"
+         */
+
+
+
+
+
+
+
+    }
+
+
+
 }
